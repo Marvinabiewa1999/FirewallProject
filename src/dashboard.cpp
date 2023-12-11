e734cb21c9279856bb2114bbdfabf08911506040
Marvinabiewa1999 <153421114+Marvinabiewa1999@users.noreply.github.com>
2023-12-10 18:24:19 -0800

    Update dashboard.cpp

--- src/dashboard.cpp
+++ src/dashboard.cpp
@@ -9,37 +9,37 @@ void Dashboard::run() {
     // Add code to display dashboard information
     std::cout << "Firewall Dashboard\n";
 
-    // Example: Display the list of blocked IPs
+    // Display the list of blocked IPs
     std::cout << "Blocked IPs:\n";
     for (const auto& ip : firewall.getBlockedIPs()) {
         std::cout << ip << "\n";
     }
 
-    // Example: Display the list of blocked outgoing ports
+    // Display the list of blocked outgoing ports
     std::cout << "Blocked Outgoing Ports:\n";
     for (const auto& port : firewall.getBlockedPortsOutgoing()) {
         std::cout << port << "\n";
     }
 
-    // Example: Display the list of blocked protocols
+    // Display the list of blocked protocols
     std::cout << "Blocked Protocols:\n";
     for (const auto& protocol : firewall.getBlockedProtocols()) {
         std::cout << protocol << "\n";
     }
 
-    // Example: Display bandwidth limits
+    // Display bandwidth limits
     std::cout << "Bandwidth Limits:\n";
     for (const auto& entry : firewall.getBandwidthLimits()) {
         std::cout << "IP: " << entry.first << ", Limit: " << entry.second << " bps\n";
     }
 
-    // Example: Display allowed countries
+    // Display allowed countries
     std::cout << "Allowed Countries:\n";
     for (const auto& country : firewall.getAllowedCountries()) {
         std::cout << country << "\n";
     }
 
-    // Example: Display blocked traffic log
+    // Display blocked traffic log
     std::cout << "Blocked Traffic Log:\n";
     for (const auto& entry : firewall.getBlockLog()) {
         std::cout << entry << "\n";
